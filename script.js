e61979d19a2689ffc68ba5e0d68d278902b2cf39
Katherine Kelly <ktkelly11@gmail.com>
2024-05-06 11:11:55 -0400

    part 1 that we worked on in class added

---
+++ script.js
@@ -0,0 +1,40 @@
+// Part 1: Humble Beginnings
+const adventurer = {
+  name: "Robin",
+  health: 10,
+  inventory: ["sword", "potion", "artifact"],
+  roll(mod = 0) {
+    const result = Math.floor(Math.random() * 20) + 1 + mod;
+    console.log(`${this.name} rolled a ${result}`);
+  },
+  companion: {
+    name: "Leo",
+    type: "Cat",
+    friend: {
+      name: "Frank",
+      type: "Flea",
+      belongings: ["small hat", "sunglasses"],
+    },
+  },
+};
+
+console.log(adventurer.name);
+console.log(adventurer.inventory[1]);
+console.log(adventurer.companion);
+console.log(adventurer.companion.friend.belongings);
+
+adventurer.inventory.forEach((item) => {
+  console.log(item);
+});
+
+console.log(adventurer);
+adventurer.roll();
+
+// Part 2: Class Fantasy
+class Character {
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    this.inventory = [];
+  }
+}
