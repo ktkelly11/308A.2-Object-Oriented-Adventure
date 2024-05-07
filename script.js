cbe76fedf8aadcc550cd496def50dabb9abe20f1
Katherine Kelly <ktkelly11@gmail.com>
2024-05-06 21:02:56 -0400

    Part 3 work started

--- script.js
+++ script.js
@@ -37,6 +37,15 @@ class Character {
     this.health = 100;
     this.inventory = [];
   }
+  roll(mod = 0) {
+    const result = Math.floor(Math.random() * 20) + 1 + mod;
+    console.log(`${this.name} rolled a ${result}`);
+  }
+  addCompanion(friend) {
+    this.companion.name = friend.name;
+    this.companion.type = friend.type;
+    this.companion.friend = companion.friend.name;
+  }
 }
 
 const robin = new Character("Robin");
@@ -48,6 +57,20 @@ robin.companion.companion.type = "Flea";
 robin.companion.companion.inventory = ["small hat", "sunglasses"];
 
 // console.log(Character.companion);
-// Character.roll();
+// Character.companion.roll();
 
 // Part 3: Class Features
+class Adventurer extends Character {
+  constructor(name, role) {
+    super(name);
+    // Adventurers have specialized roles.
+    this.role = role;
+    // Every adventurer starts with a bed and 50 gold coins.
+    this.inventory.push("bedroll", "50 gold coins");
+  }
+  // Adventurers have the ability to scout ahead of them.
+  scout() {
+    console.log(`${this.name} is scouting ahead...`);
+    super.roll();
+  }
+}
